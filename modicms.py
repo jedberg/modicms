60c345eb7c838a0f1812a4e48330623c6ba83452
Neil Williams <neil@reddit.com>
2011-04-29 21:25:37 -0700

    Add blog listing support.
    Introduces generic accumulator component:
    * blocks pipeline and collects data until
      all items are listed
    * applies function to entire listing and gets
      back metadata updates
    * goes through each collected item and applies
      updates then continues processing if necessary

--- modicms.py
+++ modicms.py
@@ -3,6 +3,7 @@ import re
 import sys
 import shutil
 import logging
+import datetime
 import cStringIO as StringIO
 
 
@@ -96,7 +97,7 @@ class Scan(_Component):
                     super(Scan, self).process(metadata, None)
                 else:
                     self.log.debug("Skipping unmodified file '%s'" % path)
-            super(Scan, self).all_items_processed()
+        super(Scan, self).all_items_processed()
 
 
 class MatchPath(_Component):
@@ -161,6 +162,71 @@ class ParseHeaders(_Component):
 
         rest = buffer.read()
         super(ParseHeaders, self).process(metadata, rest)
+
+
+class _Accumulate(_Component):
+    def __init__(self):
+        self.items = []
+        super(_Accumulate, self).__init__()
+
+    def is_invalid(self, metadata, source_mtime):
+        return True
+
+    def process(self, metadata, data):
+        self.items.append((metadata, data))
+
+    def all_items_processed(self):
+        mtime, updates = self._accumulate(self.items)
+
+        for metadata, data in self.items:
+            metadata.update(updates)
+            new_mtime = max(mtime, metadata['mtime'])
+            if super(_Accumulate, self).is_invalid(metadata, new_mtime):
+                super(_Accumulate, self).process(metadata, data)
+
+    def _accumulate(self, items):
+        raise Exception("not implemented")
+
+
+try:
+    from dateutil.parser import parse as _parse_date
+except ImportError:
+    def _parse_date(date_str):
+        return datetime.strptime(date_str, '%Y/%m/%d %H:%M:%S')
+
+
+class AccumulateBlogEntries(_Accumulate):
+    def __init__(self, path='/blog'):
+        self.path = path
+        super(AccumulateBlogEntries, self).__init__()
+
+    def _accumulate(self, items):
+        most_recent_modification = 0
+        blog_entries = []
+
+        for metadata, data in items:
+            if metadata['output_path'].startswith(self.path):
+                title = metadata.get('title', None)
+                if not title:
+                    print >>sys.stderr, "%s has no title, skipping." % (
+                                                metadata['input_path'])
+
+                mtime = metadata['mtime']
+                post_date = datetime.datetime.fromtimestamp(mtime)
+                date_str = metadata.get('date', None)
+                if date_str:
+                    post_date = _parse_date(date_str)
+
+                most_recent_modification = max(most_recent_modification, mtime)
+                blog_entries.append((post_date,
+                                     metadata['output_path'],
+                                     title))
+
+        blog_entries.sort(key=lambda x: x[0], reverse=True)
+
+        return most_recent_modification, {
+            'blog_entries': blog_entries
+        }
 
 
 try:
