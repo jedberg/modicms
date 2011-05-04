d8bc38e7e4e462f47b4df07883436c899a15394f
Neil Williams <neil@reddit.com>
2011-05-03 20:32:15 -0700

    Add CleverCSS component.

--- modicms/markup.py
+++ modicms/markup.py
@@ -2,28 +2,53 @@ import os
 
 from modicms.base import _Component
 
+class _MarkupComponent(_Component):
+    def _morph_path(self, metadata):
+        metadata = metadata.copy()
+        output_path = metadata['output_path']
+        output_root, old_extension = os.path.splitext(output_path)
+        metadata['output_path'] = output_root + self.output_extension
+        return metadata
+
+    def process(self, metadata, data):
+        morphed = self._morph_path(metadata)
+        data = self._process(morphed, data)
+        super(_MarkupComponent, self).process(morphed, data)
+
+    def _process(slf, metadata, data):
+        raise NotImplementedError()
+
+    def is_invalid(self, metadata, source_mtime):
+        morphed = self._morph_path(metadata)
+        return super(_MarkupComponent, self).is_invalid(
+            morphed, 
+            source_mtime
+        )
+
+
 try:
     import markdown
 
-    class InterpretMarkdown(_Component):
-        def _morph_path(self, metadata):
-            # change the output file's extension
-            output_root, old_extension = os.path.splitext(
-                metadata['output_path'])
-            metadata['output_path'] = output_root + '.html'
+    class InterpretMarkdown(_MarkupComponent):
+        output_extension = '.html'
 
-        def process(self, metadata, data):
-            self._morph_path(metadata)
-
+        def _process(self, metadata, data):
             md = markdown.Markdown()
             data = md.convert(data)
+            return data
 
-            super(InterpretMarkdown, self).process(metadata, data)
+except ImportError:
+    pass
 
-        def is_invalid(self, metadata, source_mtime):
-            self._morph_path(metadata)
-            return super(InterpretMarkdown, self).is_invalid(metadata,
-                                                             source_mtime)
+
+try:
+    import clevercss
+
+    class ConvertCleverCSS(_MarkupComponent):
+        output_extension = '.css'
+
+        def _process(self, metadata, data):
+            return clevercss.convert(data, minified=True)
 
 except ImportError:
     pass
